aa59841b76a81110e569d23e4b2892673a4e35c0
Thomas Erick <thomaserick.dev@gmail.com>
2025-11-07 16:11:10 -0300

    feat: adicionado backend.tf

--- infra-eks/backend.tf
+++ infra-eks/backend.tf
@@ -1,9 +1,9 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "gerenciador-oficina-fiap"
-#     key    = "eks/terraform.tfstate"
-#     region         = "us-east-1"
-#     encrypt = true
-#     use_lockfile = true
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket       = "gerenciador-oficina-fiap"
+    key          = "eks/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
